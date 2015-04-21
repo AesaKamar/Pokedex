3917fee8969304bddfa6e578554400e47a9449be
Barry Hu <hub3@rpi.edu>
2015-04-21 16:29:28 -0400

    Pokedex txt data to json

---
+++ db/txtJsonConverter.py
@@ -0,0 +1,33 @@
+import json
+import io
+import ast
+from pprint import pprint
+
+'''
+Method to turn the appropriate substrings into quoted substrings
+to be used as dictionary keys
+'''
+def colonSplicer(inStr):
+	start = 0
+	end = 0
+	for x in inStr:
+		if x == ',' or x == '{':
+			start = end+1
+			end += 1
+			continue
+		if x == ':':
+			inStr = inStr[:start]+'\"'+inStr[start:end] + '\"'+inStr[end:]
+			start = end+3
+			end+=3
+		else: end += 1
+	return inStr
+
+
+with open("pokedex.txt",'r') as f:
+	data = f.readlines()
+data2 = []
+for x in data:
+	data2.append(colonSplicer(x))
+
+with open('pokedexData.json','w') as out:
+	out.writelines(data2)
