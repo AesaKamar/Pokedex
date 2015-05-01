339a83388a6ef726234a90e43308a9e2954b9c34
Aesa Kamar <Aesa.kamar@gmail.com>
2015-05-01 02:54:22 -0400

    Removed unnecessary comments

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -19,55 +19,6 @@ class PokemonsController < ApplicationController
     # render json: @pokemon
   end
 
-  # GET /pokemons/new
-  # def new
-  #   @pokemon = Pokemon.new
-  # end
-  #
-  # # GET /pokemons/1/edit
-  # def edit
-  # end
-  #
-  # # POST /pokemons
-  # # POST /pokemons.json
-  # def create
-  #   @pokemon = Pokemon.new(pokemon_params)
-  #
-  #   respond_to do |format|
-  #     if @pokemon.save
-  #       format.html { redirect_to @pokemon, notice: 'Pokemon was successfully created.' }
-  #       format.json { render :show, status: :created, location: @pokemon }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @pokemon.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  #
-  # # PATCH/PUT /pokemons/1
-  # # PATCH/PUT /pokemons/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @pokemon.update(pokemon_params)
-  #       format.html { redirect_to @pokemon, notice: 'Pokemon was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @pokemon }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @pokemon.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  #
-  # # DELETE /pokemons/1
-  # # DELETE /pokemons/1.json
-  # def destroy
-  #   @pokemon.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to pokemons_url, notice: 'Pokemon was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pokemon
